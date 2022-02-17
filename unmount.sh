f0f49aa084e5340494ed632dd7cb593dcf08f164
bhaktiyudha <yudha.bhakti.n@gmail.com>
2022-02-17 16:29:07 +0700

    update docker-compose and mount

--- unmount.sh
+++ unmount.sh
@@ -1,4 +1,4 @@
 # Create Folder for Unmount Volumes
 # ==========================
-sudo unmount /dev/disk/by-label/WD /media/avc/WD
+sudo umount /dev/disk/by-label/WD /media/avc/WD
 sudo rm -rf /media/avc/WD/data
