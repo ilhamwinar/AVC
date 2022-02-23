e07955c4ed7c768785123af76262bc14bbbf85ea
bhaktiyudha <yudha.bhakti.n@gmail.com>
2022-02-23 10:55:27 +0700

    update makedir

--- makedir.sh
+++ makedir.sh
@@ -5,3 +5,4 @@ sudo mkdir db influxdb
 # Create Folder for Mount Volumes
 # ==========================
 sudo mkdir /media/avc/WD/data/{0,1,2,3,4,5,8,lidar}
+sudo mkdir /home/avc/data/{0,1,2,3,4,5,8,lidar}
