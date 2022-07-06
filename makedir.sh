1bcf875a0d1ccb23a1b129c0f3c27422ac7928aa
ilhamwinar <62131647+ilhamwinar@users.noreply.github.com>
2022-07-06 21:20:11 +0700

    Update makedir.sh

--- makedir.sh
+++ makedir.sh
@@ -1,11 +1,12 @@
 # Create Folder for Mount Volumes
 # ==========================
 sudo mkdir db influxdb
+sudo mkdir /home/avc/data/{0,1,2,3,4,5,8,lidar}
 
 # Create Folder for Mount Volumes
-# ==========================
-sudo mkdir /media/avc/WD/data/{0,1,2,3,4,5,8,lidar}
-sudo mkdir /home/avc/data/{0,1,2,3,4,5,8,lidar}
-sudo mkdir /home/yudha/Documents/AVC/BekasiBarat_Gardu16/                    
-sudo mkdir /home/yudha/Documents/AVC/BekasiBarat_Gardu16/data                
-sudo mkdir /home/yudha/Documents/AVC/BekasiBarat_Gardu16/data/{0,1,2,3,4,5,8}
+# # ==========================
+# sudo mkdir /media/avc/WD/data/{0,1,2,3,4,5,8,lidar}
+# sudo mkdir /home/avc/data/{0,1,2,3,4,5,8,lidar}
+# sudo mkdir /home/yudha/Documents/AVC/BekasiBarat_Gardu16/                    
+# sudo mkdir /home/yudha/Documents/AVC/BekasiBarat_Gardu16/data                
+# sudo mkdir /home/yudha/Documents/AVC/BekasiBarat_Gardu16/data/{0,1,2,3,4,5,8}
