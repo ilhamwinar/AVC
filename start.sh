e69e7bca1eaa3c79667e67694b98c9b3685fd913
ilhamwinar <62131647+ilhamwinar@users.noreply.github.com>
2022-08-23 14:13:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,7 @@ docker-compose up --detach --remove-orphans
 # Docker image prune
 # ==================
 docker image prune --all --force
+
+# Docker restart LidarAPI
+# ==================
+docker restart LidarAPI
